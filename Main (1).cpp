efc3fe2a56b0554435bdaf5767b4073f8c6216c0
om2404-rupanar <om.rupanar24@sanjivani.edu.in>
2024-08-20 16:22:15 +0530

    Add files via upload

---
+++ Main (1).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+int a = 5, b = 8;
+
+// Conditional operator
+int result = (a < b) ?b: a;
+cout << " The gretest number is " << result << endl;
+
+
+
+
+
+
+
+
+    return 0;
+}
